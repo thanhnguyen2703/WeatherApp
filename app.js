52cd285c4fd0a8627569b00f32f4993d7ef466e8
Thanh Nguyen <thanhnguyen@Thanhs-MacBook-Pro.local>
2019-11-13 21:03:31 +0200

    Add responsive greeting

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 function getTime() {
     let time = new Date();
+    let hour = 0 + time.getHours().toString();
     let minute = 0 + time.getMinutes().toString();
     let second = 0 + time.getSeconds().toString();
-    return time.getHours() + ":" + minute.slice(-2) + ":" + second.slice(-2)
+    return hour.slice(-2) + ":" + minute.slice(-2) + ":" + second.slice(-2)
 };
 
 function setTime() {
@@ -12,5 +13,16 @@ function setTime() {
 
 setInterval(() => {
     setTime()
-    console.log("hello")
+    setGreeting()
 }, 1000);
+
+function getGreeting() {
+    let time = new Date();
+    let hours = time.getHours();
+    if (hours < 12 ) { return "morning"}
+    if (hours >= 12 && hours < 18 ) { return "afternoon"}
+    if (hours > 18) { return "evening"} 
+}
+function setGreeting() {
+    document.getElementById("greeting").textContent = getGreeting()
+}
